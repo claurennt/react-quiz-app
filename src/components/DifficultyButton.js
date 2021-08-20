d5452c4cf3021caa183a16f8594fa83c10514364
claurennt <c.benedetti1989@gmail.com>
2021-08-20 14:49:09 +0200

    create button component

---
+++ src/components/DifficultyButton.js
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import QuestionsContext from '../context/QuestionsContext';
+
+const DifficultyButton = ({ level }) => {
+  const {
+    difficulty: { setDifficulty },
+  } = useContext(QuestionsContext);
+
+  return (
+    <>
+      {/* change difficulty state based on prop*/}
+      <button className='level-button' onClick={() => setDifficulty(level)}>
+        {level} {/* display difficulty level based on prop */}
+      </button>
+    </>
+  );
+};
+
+export default DifficultyButton;
