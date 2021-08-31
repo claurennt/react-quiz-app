a3b5cfb3823b44bb81594c98a03dddff2b9ab673
claurennt <c.benedetti1989@gmail.com>
2021-08-31 12:20:06 +0200

    turn select answer button into normal button

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,11 +1,7 @@
 const SelectAnswer = ({ option }) => {
   return (
     <>
-      <input type='radio' id={option} name='choice' />
-      <label className='select-option' for={option}>
-        {option}
-      </label>
-      <br />
+      <button className='option-button'>{option}</button>
     </>
   );
 };
